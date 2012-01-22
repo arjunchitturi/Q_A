39f8d67027bbf48228fed64ada13f5209f300d5d
Venkata Ramana C <arjunchitturi@gmail.com>
2012-01-22 17:12:41 +0530

    added admin models

--- djangobase/bootstrap/admin.py
+++ djangobase/bootstrap/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+#from bootstrap.forms import AnswerModelForm
 
-from bootstrap.models import StarWarsCharacter
+from bootstrap.models import User, Question, Answer#, StarWarsCharacter
 
-admin.site.register(StarWarsCharacter)
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 1
+    exclude = ('added_at',)    
+
+class QuestionInline(admin.ModelAdmin):
+    inlines = [ AnswerInline, ]
+    exclude = ('added_at',)
+
+admin.site.register(User)
+admin.site.register(Question, QuestionInline)
+
+#admin.site.register(Answer)
+#admin.site.register(StarWarsCharacter)
+
